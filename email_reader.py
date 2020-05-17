c7f19142f9fc95bbacfbd74534a77a0bde3c8ab5
Louis <koo2434@gmail.com>
2020-05-16 22:23:42 -0500

    sender email address parsing

--- email_reader.py
+++ email_reader.py
@@ -1,12 +1,9 @@
 from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
 import pickle
 import os.path
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
 import base64
 from apiclient import errors
-
-import threading
 import time
 
 SCOPES = ['https://mail.google.com/']
@@ -16,6 +13,7 @@ class GmailListener:
         self.creds = None
         self.service = None
         self.recentHistoryId = -1
+        self.authorizedAccounts = []
 
     def set_creds_service(self):
         if os.path.exists('token.pickle'):
@@ -39,6 +37,11 @@ class GmailListener:
             recentMsg = self.service.users().messages().get(userId='me', id = messages[0]['id']).execute()
             self.recentHistoryId = recentMsg['historyId']
 
+    def set_authorized_accounts(self):
+        with open('authorized_accounts.txt', 'rt') as accounts:
+            self.authorizedAccounts = [account.strip() for account in accounts]
+        print(self.authorizedAccounts[0])
+
     def get_new_emails(self):
         try:
             for _ in range (10):
@@ -48,10 +51,35 @@ class GmailListener:
                     print("ID {0}: No new messages.".format(self.recentHistoryId))
                 else:
                     latestNewMessage = messages[-1]
-                    id = latestMessage.get('messages')[0].get('id')
+                    id = latestNewMessage.get('messages')[0].get('id')
                     recentMsg = self.service.users().messages().get(userId='me', id = id).execute()
-                    recentMsgBody = recentMsg['snippet']
-                    self.recentHistoryId = recentMsgBody['historyId']
+                    recentMsgBody = recentMsg['snippet'].strip()
+                    self.recentHistoryId = recentMsg['historyId']
+
+                    print(recentMsgBody)
+                time.sleep(3)
+        except errors.HttpError as error:
+            print(error)
+
+    def test(self):
+        try:
+            for _ in range (10):
+                results = self.service.users().history().list(userId='me', startHistoryId=self.recentHistoryId).execute()
+                messages = results.get('history', [])
+                if not messages:
+                    print("ID {0}: No new messages.".format(self.recentHistoryId))
+                else:
+                    latestNewMessage = messages[-1]
+                    id = latestNewMessage.get('messages')[0].get('id')
+                    recentMsg = self.service.users().messages().get(userId='me', id = id).execute()
+                    recentMsgEmail = self.service.users().messages().get(userId='me', id = id, format='metadata',
+                                            metadataHeaders=['From'], fields='payload/headers').execute()
+                    recentMsgEmail = recentMsgEmail.get('payload').get('headers')[0].get('value')
+                    recentMsgBody = recentMsg.get('snippet').strip()
+                    self.recentHistoryId = recentMsg.get('historyId')
+
+                    print(recentMsgEmail)
+                    print(recentMsgBody)
                 time.sleep(3)
         except errors.HttpError as error:
             print(error)
@@ -60,4 +88,6 @@ if __name__ == '__main__':
     listener = GmailListener()
     listener.set_creds_service()
     listener.set_recent_id()
-    listener.get_new_emails()
+    #listener.set_authorized_accounts()
+    #listener.get_new_emails()
+    listener.test()
